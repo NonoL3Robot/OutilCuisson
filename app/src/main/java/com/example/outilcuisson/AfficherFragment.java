b0b7bb55da955beb6956271d0e066aad0d5049ee
NonoL3Robot <no.way.l3.robot+github@gmail.com>
2022-02-21 12:09:29 +0100

    Ajout des fichiers à coder

---
+++ app/src/main/java/com/example/outilcuisson/AfficherFragment.java
@@ -0,0 +1,12 @@
+/*
+ * AfficherFragment.java, 21/02/2022
+ * IUT Rodez 2022-2022, INFO2
+ * pas de copyright, aucun droits
+ */
+
+package com.example.outilcuisson;
+
+/**
+ * TODO class comment
+ */
+public class AfficherFragment {}
