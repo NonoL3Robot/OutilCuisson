96af2c51de957eb6ac5e1fe478e45ef33cb5a49c
NonoL3Robot <no.way.l3.robot+github@gmail.com>
2022-02-22 12:20:11 +0100

    optimize import

--- app/src/main/java/com/example/outilcuisson/AdaptateurPage.java
+++ app/src/main/java/com/example/outilcuisson/AdaptateurPage.java
@@ -7,7 +7,6 @@
 package com.example.outilcuisson;
 
 import androidx.fragment.app.Fragment;
-
 import androidx.fragment.app.FragmentActivity;
 import androidx.viewpager2.adapter.FragmentStateAdapter;
 
